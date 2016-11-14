3ab0ed3c58875f46df42e593b4b85224b41efe25
James Ashmore <s1437643@sms.ed.ac.uk>
2016-11-14 18:39:24 +0000

    Commit

---
+++ list.c
@@ -0,0 +1,32 @@
+typedef struct node
+{
+    int n;
+    struct node *next;
+}
+node;
+
+
+/*
+ *
+ */
+node * create(void)
+{
+    node *ptr = NULL;
+    return ptr;
+}
+
+/*
+ *
+ */
+void insert()
+{
+    
+}
+
+/*
+ *
+ */
+void delete()
+{
+    
+}
